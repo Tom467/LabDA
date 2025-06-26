d89f4b4d5547021d05c9e25b05cb8ede09206af1
Hollis Williams <40658473+Tom467@users.noreply.github.com>
2025-06-26 19:06:06 +0900

    Delete plotter.py

--- plotter.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-
-def plot(x_parameter, y_parameter):
-    plt.plot(x_parameter.value, y_parameter.value)
-    plt.xlabel(f'{x_parameter.name} ({x_parameter.units})')
-    plt.ylabel(f'{y_parameter.name} ({y_parameter.units})')
