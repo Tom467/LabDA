c800f61a87e10b9133f22276a5880016b08564ea
Hollis Williams <40658473+Tom467@users.noreply.github.com>
2025-06-26 17:53:29 +0900

    Update LabDAinterface.py

--- LabDAinterface.py
+++ LabDAinterface.py
@@ -192,7 +192,7 @@ if __name__ == '__main__':
 
 
     class DimensionalMatrix:
-    def __init__(self, parameters):  # parameters should be of type ListOfParameters or ListOfUnits
+            def __init__(self, parameters):  # parameters should be of type ListOfParameters or ListOfUnits
         self.parameters = parameters if isinstance(parameters, ListOfParameters) else ListOfParameters(parameters)
         self.M = DimensionalMatrix.create_dimensional_matrix(self.parameters.units)
         self.rank = np.linalg.matrix_rank(self.M) if len(self.M) > 0 else 0
