6fabc05cd8856bdc6bf6ddb37c22e3803b6a0d13
Hollis Williams <40658473+Tom467@users.noreply.github.com>
2025-06-27 17:30:29 +0900

    Update Interfacev3.py

--- Interfacev3.py
+++ Interfacev3.py
@@ -109,6 +109,66 @@ class DimensionalMatrix:
 
 
 
+class PiGroup:
+    def __init__(self, parameters):  # parameters should be of type ListOfParameters with the first parameter plus repeating variables
+        self.parameters = parameters
+        self.values = None
+        self.exponents = None
+        self._define_pi_group()
+        self.formula = None
+        self.formula_inverse = None
+        self._define_formula()
+        self.repeating_variables = parameters[1:]
+        # TODO add some check to see if the Pi group is something common like the Reynold's Number
+
+    def __str__(self):
+        return self.formula  # str(self.values) + ' ' + str(self.formula)
+
+    def __eq__(self, other):
+        return self.formula == other.formula or self.formula == other.formula_inverse
+
+    def _define_pi_group(self):
+        M = DimensionalMatrix(self.parameters.units).M
+        A, B = M[:, 1:], M[:, 0]
+        self.exponents = np.round(-(np.linalg.inv(A) @ B), 2)  # TODO rounding might cause problems with small fractions
+        self.values = self.calculate_value(self.parameters)
+        # TODO add logic to make sure x is a vector of integers if raising units to this power
+
+    def calculate_value(self, parameters):
+        value = copy.deepcopy(parameters[0].values)
+        for i, parameter in enumerate(parameters[1:]):
+            value *= parameter.values**self.exponents[i]
+        return value
+        # TODO figure out what to return in addition to the total
+
+    def contains(self, other_name):
+        for param in self.parameters:
+            if param.name == other_name:
+                return True
+        return False
+
+    def _define_formula(self):
+        top = ''
+        bottom = ''
+        for i, parameter in enumerate(self.parameters):
+            if i == 0:
+                top += f'({parameter.name})'
+            else:
+                if self.exponents[i-1] > 0:
+                    if self.exponents[i-1] == 1:
+                        top += f'({parameter.name})'
+                    else:
+                        top += f'({parameter.name}^'+'{'+f'{self.exponents[i-1]}'+'})'
+                elif self.exponents[i-1] < 0:
+                    if self.exponents[i-1] == -1:
+                        bottom += f'({parameter.name})'
+                    else:
+                        bottom += f'({parameter.name}^'+'{'+f'{-self.exponents[i-1]}'+'})'
+
+        self.formula = r'$\frac{t}{b}$'.replace('t', top).replace('b', bottom) if bottom else top
+        self.formula_inverse = f'{bottom} / {top}' if top else bottom
+
+
 
 
 
