4b85d74c7294a757c484619e8c91f71a94df33eb
Hollis Williams <40658473+Tom467@users.noreply.github.com>
2025-06-27 17:44:09 +0900

    Update Interfacev3.py

--- Interfacev3.py
+++ Interfacev3.py
@@ -269,16 +269,19 @@ def generate_plots(dimensional_analysis):
     st.write("Generated plots for Pi group sets")
     for i, pi_set in enumerate(dimensional_analysis.pi_group_sets):
         with st.expander(f"Group {i+1}"):
-            st.write(f"Repeating variables: {[p.name for p in pi_set]}")
+            st.write(f"Repeating variables: {[p.name for p in pi_set.repeating_variables]}")
 
-            st.write("Sample values for each variable:")
-            for p in pi_set:
+            st.write("Sample values for each repeating variable:")
+            for p in pi_set.repeating_variables:
                 st.write(f"{p.name}: {p.values[:5]} ({p.units})")
 
             if len(pi_set) >= 2:
                 st.pyplot(plot(pi_set[0], pi_set[1]))
             else:
-                st.info("Not enough parameters to plot.")
+                st.info("Not enough Pi groups to plot.")
+
+
+
 
 def find_contours(img, threshold1=100, threshold2=200, blur=3):
     img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
