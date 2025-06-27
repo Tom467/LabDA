fefa6ccbf301ef6362eda1ef85b5e8385a6894dd
Hollis Williams <40658473+Tom467@users.noreply.github.com>
2025-06-27 11:48:16 +0900

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -134,7 +134,7 @@ def find_contours(img, threshold1=100, threshold2=200, blur=3):
     return cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE), edges
 
 # --- Streamlit UI ---
-st.title("Unified Dimensional Analysis App")
+st.title("LabDA: User-friendly Dimensional Analysis")
 option = st.sidebar.selectbox('Select input type', ('CSV File', 'Images'))
 
 if option == 'CSV File':
